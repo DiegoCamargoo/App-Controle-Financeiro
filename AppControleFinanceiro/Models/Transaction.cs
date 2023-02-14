ec7d2fbcfaeb75eb90b17ddf2d7bd9d6a271ab63
Diego <tiDiegoCamargo@gmail.com>
2023-02-13 22:40:56 -0300

    feat: criação repositorio

--- AppControleFinanceiro/Models/Transaction.cs
+++ AppControleFinanceiro/Models/Transaction.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LiteDB;
 
 namespace AppControleFinanceiro.Models;
 
 public class Transaction
 {
+    [BsonId]
     public int Id { get; set; }
     public TransactionType Type { get; set; }
     public string Name { get; set; }
---
+++ AppControleFinanceiro/Repositories/TransactionRepository.cs
@@ -0,0 +1,49 @@
+﻿using AppControleFinanceiro.Models;
+using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppControleFinanceiro.Repositories
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        private readonly LiteDatabase _database;
+        private readonly string collectionName = "transactions";
+        public TransactionRepository()
+        {
+            _database = new LiteDatabase("Filenam=C:/users/AppData/database.db;Connection=Shared");
+        }
+        public List<Transaction> GetAll()
+        {
+            return _database
+                .GetCollection<Transaction>(collectionName)
+                .Query()
+                .OrderByDescending(p => p.Date)
+                .ToList();
+
+        }
+        public void Add(Transaction transaction)
+        {
+            var col = _database.GetCollection<Transaction>(collectionName);
+                col.Insert(transaction);
+                col.EnsureIndex(i => i.Date);
+
+        }
+        public void Update(Transaction transaction)
+        {
+            var col = _database.GetCollection<Transaction>(collectionName);
+            col.Update(transaction);
+
+        }
+        public void Delete(Transaction transaction)
+        {
+            var col = _database.GetCollection<Transaction>(collectionName);
+            col.Delete(transaction.Id);
+        }
+
+
+    }
+}
